7ad545d81b8fb50b48e4dc277925efbe203821a6
Ivan Carlos de Paula <ivancarlosdepaula@outlook.com>
2022-11-09 14:51:06 -0300

    Não tá funcionando direito

--- src/screens/Groups/styles.ts
+++ src/screens/Groups/styles.ts
@@ -1,2 +1,13 @@
-import styled from 'styled-components';
+import styled from 'styled-components/native';
 
+export const Container = styled.View`
+  flex: 1;
+  background-color: #121212;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const Title = styled.Text`
+  color: #fff;
+
+`;
