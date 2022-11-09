09cc3deb7c04b0f4d49be987c4085daef6fd733a
Ivan Carlos de Paula <ivancarlosdepaula@outlook.com>
2022-11-09 18:07:31 -0300

    statusbar

--- App.tsx
+++ App.tsx
@@ -1,5 +1,6 @@
-import {ThemeProvider} from 'styled-components';
-import {useFonts, Roboto_400Regular, Roboto_700Bold} from '@expo-google-fonts/roboto';
+import { StatusBar } from 'react-native';
+import { ThemeProvider } from 'styled-components';
+import { useFonts, Roboto_400Regular, Roboto_700Bold}  from '@expo-google-fonts/roboto';
 
 import { Groups } from '@screens/Groups';
 
@@ -14,6 +15,11 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
+      <StatusBar
+        barStyle='light-content'
+        backgroundColor='transparent'
+        translucent
+      />
       { fontsLoaded ?  <Groups /> : <Loading />}
     </ThemeProvider>
   );
