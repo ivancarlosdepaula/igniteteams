e4fa98b4503ee04a13a5e975c038f7c752c92227
Ivan Carlos de Paula <ivancarlosdepaula@outlook.com>
2022-11-09 18:00:44 -0300

    Loading

--- App.tsx
+++ App.tsx
@@ -1,13 +1,20 @@
 import {ThemeProvider} from 'styled-components';
+import {useFonts, Roboto_400Regular, Roboto_700Bold} from '@expo-google-fonts/roboto';
 
 import { Groups } from '@screens/Groups';
 
 import theme from './src/theme';
+import { Loading } from '@components/Loading';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold
+  });
+
   return (
     <ThemeProvider theme={theme}>
-      <Groups />
+      { fontsLoaded ?  <Groups /> : <Loading />}
     </ThemeProvider>
   );
 }
---
+++ src/components/Loading/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import {
+  Container,
+  LoadingIndicator
+ } from './styles';
+
+const Loading =  () => {
+  return (
+    <Container>
+      <LoadingIndicator />
+    </Container>
+  );
+}
+
+export { Loading };
