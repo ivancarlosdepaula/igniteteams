05cc19193700b6f7e86023bc1f69778a3ff58d85
Ivan Carlos de Paula <ivancarlosdepaula@outlook.com>
2022-11-09 16:31:14 -0300

    temas

--- src/screens/Groups/styles.ts
+++ src/screens/Groups/styles.ts
@@ -1,8 +1,10 @@
 import styled from 'styled-components/native';
 
+
+
 export const Container = styled.View`
   flex: 1;
-  background-color: #121212;
+  background-color: ${({theme})=>theme.COLORS.GRAY_600};
   align-items: center;
   justify-content: center;
 `;
