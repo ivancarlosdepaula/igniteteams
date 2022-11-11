cc6604a7faac00c2e87729a611b5b607cdbc0d89
Ivan Carlos de Paula <ivancarlosdepaula@outlook.com>
2022-11-11 13:55:45 -0300

    Componente Filter

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -4,6 +4,7 @@ import { ButtonIcon } from '@components/ButtonIcon';
 import { Input } from '@components/Input';
 
 import { Container, Form } from './styles';
+import { Filter } from '@components/Filter';
 
 export function Players(){
   return (
@@ -17,6 +18,10 @@ export function Players(){
         <Input placeholder='Nome da pessoa' autoCorrect={false} />
         <ButtonIcon icon='add' type='PRIMARY' />
       </Form>
+      <Filter
+        title='Time A'
+        isActive={true}
+      />
     </Container>
   );
 }
