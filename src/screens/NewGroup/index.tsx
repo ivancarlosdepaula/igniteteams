c3a417f2e12f3839f392dba46dab055bb72d9e61
Ivan Carlos de Paula <ivancarlosdepaula@outlook.com>
2022-11-11 08:47:32 -0300

    Input

--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -1,6 +1,7 @@
-import { Button } from '@components/Button';
 import { Header } from '@components/Header';
 import { Highlight } from '@components/Highlight';
+import { Input } from '@components/Input';
+import { Button } from '@components/Button';
 
 import {
  Container,
@@ -15,7 +16,8 @@ export function NewGroup(){
       <Content>
         <Icon />
         <Highlight title="Nova turma" subtitle="Crie a turma para adicionar as pessoas" />
-        <Button title="Criar" />
+        <Input  />
+        <Button title="Criar" style={{marginTop: 20}} />
       </Content>
     </Container>
   );
